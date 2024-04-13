6994840b72bc7e5029d350987ebf2e960f1d5a92
pi5user <pi5user@163.com>
2024-04-13 08:49:30 +0800

    public_test

---
+++ public_test.sh
@@ -0,0 +1,9 @@
+echo "# public_test" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin git@github.com:lqingshun/public_test.git
+git push -u origin master
+
+## Sat 13 Apr 08:48:20 CST 2024
