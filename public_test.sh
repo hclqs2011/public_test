33732a26ebf91f9aad75c51ce991e9100fb145f5
hclqs2011 <166883847+hclqs2011@users.noreply.github.com>
2024-04-13 09:24:39 +0800

    Update public_test.sh modify comment

--- public_test.sh
+++ public_test.sh
@@ -1,7 +1,7 @@
 echo "# public_test" >> README.md
 git init
 git add README.md
-git commit -m "first commit"
+git commit -m "first commit modify commit "
 git branch -M master
 git remote add origin git@github.com:lqingshun/public_test.git
 git push -u origin master
